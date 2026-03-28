d933fe044311991063f76033575c299398ed3a13
Horváth Gergő <hogeri14@gmail.com>
2026-03-28 15:55:07 +0100

    initialize ORM setup with SQLAlchemy and Flask-Migrate; add requirements file

--- hibavonal-be/app.py
+++ hibavonal-be/app.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify
 from flasgger import Swagger
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///hibavonal.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 swagger = Swagger(app)
 
