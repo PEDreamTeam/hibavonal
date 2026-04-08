231d9c65f31dc89bc4358d29f410f1a24f63e6fd
Horváth Gergő <hogeri14@gmail.com>
2026-04-08 18:16:51 +0200

    add: implement initial structure for Tickets component with necessary imports

---
+++ hibavonal-fe/src/pages/tickets/Tickets.jsx
@@ -0,0 +1,21 @@
+import { useMemo, useState } from 'react';
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    Container,
+    Divider,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Menu,
+    MenuItem,
+    Stack,
+    Typography,
+    Chip,
+} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
