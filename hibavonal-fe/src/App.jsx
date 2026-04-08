5cc1068d9e2d7b317fa9d049e1c4e24f29082e5a
Horváth Gergő <hogeri14@gmail.com>
2026-04-08 18:57:18 +0200

    add: import Tickets component in App for routing

--- hibavonal-fe/src/App.jsx
+++ hibavonal-fe/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/home/Home';
+import Tickets from './pages/tickets/Tickets';
 import Login from './pages/login/Login';
 
 function App() {
