9b442a43286d844446fc66e2a003251a49b8c89e
GergoViolanist <1990gergo.hegedus@gmail.com>
2026-04-17 17:15:03 +0200

    feat: register tickets blueprint in Flask app

--- hibavonal-be/app.py
+++ hibavonal-be/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flasgger import Swagger
+from routes.tickets import tickets_bp
 
 app = Flask(__name__)
 
 swagger = Swagger(app)
+
+# Register blueprints
+app.register_blueprint(tickets_bp)
 
 @app.route("/", methods=["GET"])
 def index():
