ee273bac218e7ef8ccc85ca807c638e8299deaec
GergoViolanist <1990gergo.hegedus@gmail.com>
2026-04-24 12:22:25 +0200

    Add import for AddTicketTypeForm in App component

--- hibavonal-fe/src/App.jsx
+++ hibavonal-fe/src/App.jsx
@@ -7,6 +7,7 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import ToolOrderForm from './pages/tool-order/ToolOrderForm';
 import Rooms from './pages/rooms/Rooms';
+import AddTicketTypeForm from './pages/ticket-types/AddTicketTypeForm';
 
 const App = () => {
   return (
